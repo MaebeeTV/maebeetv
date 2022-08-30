9ec19c228e646a14889edfea31b12f260a1d6115
amy <ctrlalternativedelete@gmail.com>
2022-08-30 17:46:58 +1000

    added transitions

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,8 +15,6 @@ const Navbar: NextPage = () => {
 
     const navbar_options = found_route?.navbar_options;
 
-    console.log(navbar_options?.bg_color)
-
     return (
         <nav className={`bg-${navbar_options?.bg_color ? navbar_options.bg_color : "[#FF9DD0]"} sticky top-0 z-50 md:mt-0 md:py-0 py-3 text-${ navbar_options?.text_color ? navbar_options.text_color : "black" }`}>
             <div className="container flex flex-wrap justify-between mx-auto items-stretch gap-3">
@@ -26,15 +24,15 @@ const Navbar: NextPage = () => {
                         <span className="">MaebeeTV Productions</span>
                     </a>
                 </Link>
-                <button className="md:hidden px-3" title="Navigation" onClick={() => { setNavbar(!navbar) }}>
+                <button className="transition-all md:hidden px-3" title="Navigation" onClick={() => { setNavbar(!navbar) }}>
                     <Bars3Icon height="32px" />
                 </button>
-                <ul className={`md:flex ${navbar ? "" : "hidden"} items-stretch md:w-auto w-full`}>
+                <ul className={`transition-all md:flex ${navbar ? "" : "hidden"} items-stretch md:w-auto w-full`}>
                     {
                         routes.map((e) => {
                             if (e.show_in_nav) {
                                 return (
-                                    <li key={e.path} className={`flex flex-1 items-stretch hover:backdrop-brightness-90 ${e.path == router.route ? "backdrop-brightness-125" : ""}`}>
+                                    <li key={e.path} className={`transition-all flex flex-1 items-stretch hover:backdrop-brightness-90 ${e.path == router.route ? "backdrop-brightness-125" : ""}`}>
                                         <Link href={e.path}>
                                             <a className="flex flex-1 justify-center items-center p-3 md whitespace-nowrap">
                                                 {e.name}
