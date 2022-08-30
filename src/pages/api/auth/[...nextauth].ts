50cfa5699e307030ff37c6ca7a2e29f2ae820176
amy <ctrlalternativedelete@gmail.com>
2022-08-30 20:32:21 +1000

    added login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,9 +6,26 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../server/db/client";
 import { env } from "../../../env/server.mjs";
 
+import axios from "axios";
+
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
+    async signIn({ account }) {
+      if (account.provider === "discord") {
+        const guilds: any[] = (await axios.get("https://discord.com/api/users/@me/guilds", 
+          {
+            headers: {
+              Authorization: `Bearer ${account.access_token}`
+            }
+          }
+        )).data as any[];
+        const guild_ids = guilds.flatMap(e => e.id as string);
+        console.log(guild_ids)
+        return (guild_ids.findIndex(e => e === process.env.DISCORD_ALLOWED_GUILD) !== -1);
+      }
+      return false;
+    },
     session({ session, user }) {
       if (session.user) {
         session.user.id = user.id;
@@ -22,6 +39,7 @@ export const authOptions: NextAuthOptions = {
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
+      authorization: { params: { scope: 'identify guilds email' } },
     }),
     // ...add more providers here
   ],
