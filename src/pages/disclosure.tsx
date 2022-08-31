ef3c8d320895ee20a892c8aee1dc24b905b8f302
amy <ctrlalternativedelete@gmail.com>
2022-08-31 15:24:47 +1000

    added headless ui

---
+++ src/pages/disclosure.tsx
@@ -0,0 +1,18 @@
+import { Disclosure } from '@headlessui/react'
+import { NextPage } from 'next'
+
+const MyDisclosure: NextPage = () => {
+  return (
+    <Disclosure>
+      <Disclosure.Button className="py-2">
+        Is team pricing available?
+      </Disclosure.Button>
+      <Disclosure.Panel className="text-gray-500">
+        Yes! You can purchase a license that you can share with your entire
+        team.
+      </Disclosure.Panel>
+    </Disclosure>
+  )
+}
+
+export default MyDisclosure;
