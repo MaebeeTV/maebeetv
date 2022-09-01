41d5d839de021c800d1bea8de80a9f613ae3f593
amy <ctrlalternativedelete@gmail.com>
2022-09-01 11:16:45 +1000

    fixed card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,9 +4,9 @@ const Card: FC<{children: React.ReactNode, title?: string, className?: string}> 
     return (
         <div className={`block p-6 rounded-lg shadow-lg dark:backdrop-brightness-200 max-w-sm ${className}`}>
             <h5 className="text-gray-900 dark:text-gray-200 text-xl leading-tight font-medium mb-2">{title}</h5>
-            <p className="text-gray-700 dark:text-gray-400 text-base">
+            <div className="text-gray-700 dark:text-gray-400 text-base">
                 {children}
-            </p>
+            </div>
         </div>
     )
 }
