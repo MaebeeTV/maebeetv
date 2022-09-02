a91198fd6b90ec14455659684365b38b6e3db4f8
amy <ctrlalternativedelete@gmail.com>
2022-09-02 12:09:19 +1000

    added dashboard text

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,30 @@
+import Card from "components/Card";
+import Spinner from "components/Spinner";
 import DashboardLayout from "layout/dashboard";
 import { NextPageWithLayout } from "next";
+import { useSession } from "next-auth/react";
 import { ReactElement } from "react";
 
 const Dashboard: NextPageWithLayout = () => {
-    return <></>
+    const { data: session, status } = useSession();
+
+    if (status === "loading" || !session?.user) {
+        return (
+            <div className="flex-1 flex justify-center items-center">
+                <div>
+                    <Spinner className="h-16 w-16" />
+                </div>
+            </div>
+        );
+    }
+    
+    return (
+        <div className="flex-1 flex items-center justify-center">
+            <Card title={`Welcome, ${session.user.discordName}.`}>
+                &quot;we got your data now sod off ~ ash 2022&quot;
+            </Card>
+        </div>
+    )
 }
 
 Dashboard.getLayout = (page) => {
